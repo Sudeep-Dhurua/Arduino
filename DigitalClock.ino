edb6c1e0a0c1e5375d6e6fd0930ecce1ed25613b
Sudeep Kumar Dhurua <69045979+Sudeep-Dhurua@users.noreply.github.com>
2023-12-21 23:22:08 +0530

    Update DigitalClock.ino
    2nd

--- DigitalClock.ino
+++ DigitalClock.ino
@@ -12,3 +12,35 @@
 #define SCREEN_ADDRESS 0x3C
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+const char *ssid = "ROMBU_TEST"; //Setting a username
+const char *password = "1234567"; //Setting the password
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+String weekDays[7] = {"Sun" , "Mon" , "Tue" , "Wed" , "Thr" , "Fri" , "Sat"};
+String months[12] = {"Jan" , "Feb" , "Mar" , "Apr" , "May" , "Jun" , "Jul" , "Aug" , "Sept" , "Oct" , "Nov" , "Dec"};
+
+const int ledPin = D10;
+const int buzzerPin = D9;
+
+const int activateHour = 10;
+const int activateMinute = 38;
+const int deactivateHour = 10;
+const int deactivateMinute = 39;
+
+char intro[] = "GIVE THIS VIDEO A LIKE, IF YOU ENJOYED!, | FROM SUDEEP";
+int x, minX;
+
+bool deviceActive = false;
+
+void wifiConnect() {
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.clearDisplay();
+  display.setCursor(0, 10);
+  display.println("WiFi");
+  display.setCursor(0, 30);
+  display.println("Connecting");
+  display.display();
